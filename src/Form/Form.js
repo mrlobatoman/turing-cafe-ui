2ef9c2d7c87b553b70603c0ad7e78771cb83b89b
Michael Lobato <mike.lobato.wft@gmail.com>
2023-05-30 08:32:41 -0700

    Add: input field information

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -14,16 +14,37 @@ class Form extends Component {
     render() {
         return (
             <form>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
+                <input 
+                    type='text'
+                    placeholder='Name'
+                    name='name'
+                    value={this.state.name}
+                    onChange={event => this.handleChange(event)}
+                />
+                <input
+                    type='text'
+                    placeholder='Date'
+                    name='date'
+                    value={this.state.date}
+                    onChange={event => this.handleChange(event)}
+                />
+                <input
+                    type='text'
+                    placeholder='Time'
+                    name='time'
+                    value={this.state.time}
+                    onChange={event => this.handleChange(event)}
+                />
+                <input
+                    type='text'
+                    placeholder='Number of guests'
+                    name='number of guests'
+                    value={this.state.numberOfGuests}
+                    onChange={event => this.handleChange(event)}
+                />
             </form>
         )
     }
 }
-
-
-
 
 export default Form
